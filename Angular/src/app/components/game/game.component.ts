1111930da208cde3988cab85d5d43837ac296106
Alex <alexbss1996@gmail.com>
2022-06-29 17:15:06 +0200

    link game, game random, interfacce movie

--- Angular/src/app/components/game/game.component.ts
+++ Angular/src/app/components/game/game.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Movie, MovieGame } from 'src/app/models/movie';
 
 @Component({
   selector: 'tnv-game',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  movie = {};
+  @Input() movies: Movie[] = [];
+  
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getRandomMovie();
   }
 
+  getRandomMovie() {
+    // Per determinare questo valore facciamo eventualmente una query su movies/latest per avere l'id dell'ultimo Film inserito su TMDB
+    const latestId = 30000;
+    const randomId = Math.round(Math.random() * latestId);
+
+      this.http
+      .get(
+        `https://api.themoviedb.org/3/movie/${randomId}?api_key=3949444e64e7a9355250d3b1b5c59bf1&language=it-it`
+      )
+      .subscribe({
+        // Qui non usate any ovviamente, ma create l'interfaccia typescript per la response
+        next: (res: Partial<Movie>) => {
+          console.log('ID trovato', randomId);
+          if (res.poster_path) {
+            this.movie = res;
+          } else {
+            console.log('Film senza poster');
+            this.getRandomMovie();
+          }
+        },
+        error: () => {
+          console.log('ID non esistente, retry!', randomId);
+          this.getRandomMovie();
+        },
+      });
+  }
 }
