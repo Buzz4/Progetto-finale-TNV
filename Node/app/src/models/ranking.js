b16561ace9cffd3afb7d7f2b63d3dc36a7b93b98
Francesca Lusso <francesca.lusso9988@gmail.com>
2022-07-01 15:37:25 +0200

    Creazione favorite e ranking NODE

---
+++ Node/app/src/models/ranking.js
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize"; 
+import db from "../../config/config.js";
+ 
+const { DataTypes } = Sequelize;
+
+const Rating = db.define('ranking', {
+  userId: {
+    type: DataTypes.INTEGER
+  },
+  movieId: {
+    type: DataTypes.INTEGER
+  },
+  gamePoints: {
+    type: DataTypes.INTEGER
+  }
+}, {
+  freezeTableName: true
+});
+ 
+export default Rating;
